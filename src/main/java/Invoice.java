44a1d137fb095697889d80320ff9b7876fde70c5
BaraahAlKalbani <baraalkalbani@gmail.com>
2023-01-31 09:55:10 +0400

    commit

--- src/main/java/Invoice.java
+++ src/main/java/Invoice.java
@@ -1,6 +1,15 @@
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
 
 public class Invoice {
+    
+
+    private ArrayList<ShopSetting> header;
     private String customerName;
     private String phoneNumber;
     private String invoiceDate;
@@ -19,6 +28,80 @@ public class Invoice {
         calculateTotalAmount();
     }
     
+    public void newInvoice() {
+        Gson gson = new Gson();
+        FileReader reader = new FileReader("data/Items.json");
+        HashMap<Integer, Item> items = gson.fromJson(reader, HashMap.class);
+        reader.close();
+        
+        reader = new FileReader("data/shopSettingData.json");
+        ShopSetting invoiceHeader = gson.fromJson(reader, ShopSetting.class);
+        reader.close();
+        
+        
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Enter customer name: ");
+        String customerName = userInput.nextLine();
+        System.out.print("Enter customer phone number: ");
+        String phoneNumber = userInput.nextLine();
+
+        // Get item information
+        System.out.print("Enter number of items: ");
+        
+        Integer numberOfItems = Integer.parseInt(userInput.nextLine());
+        Item[] invoiceItems = new Item[numberOfItems];
+        double totalAmount = 0;
+        for (int i = 0; i < numberOfItems; i++) {
+            System.out.print("Enter item id: ");
+            int itemId = Integer.parseInt(userInput.nextLine());
+            Iterator<Item> iterator = items.iterator();
+            Boolean idFound =false;
+            while (iterator.hasNext()) {
+                Item item = iterator.next();
+                if (item.getItemID() == id) {
+                    iterator.remove();
+                    idFound = true;
+                    System.out.println("Item removed successfully.");
+                    break;
+                }
+            }
+        }
+    }
+    public ArrayList<ShopSetting> getHeader() {
+        return header;
+    }
+
+    public void setHeader(ArrayList<ShopSetting> header) {
+        this.header = header;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setInvoiceDate(String invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
+
+    public void setNumberOfItems(int numberOfItems) {
+        this.numberOfItems = numberOfItems;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+    }
     private void calculateNumberOfItems() {
         numberOfItems = items.size();
     }
@@ -69,4 +152,5 @@ public class Invoice {
     public ArrayList<Item> getItems() {
         return items;
     }
+    
 }
